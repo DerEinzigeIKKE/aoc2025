49c5a9c86e831b7b2380dc1ceabb79e418d17ac3
DerEinzigeIKKE <sral.hanselmann@gmail.com>
2026-01-01 23:00:18 +0100

    implemented standard code for development

--- Day02_Quest2.py
+++ Day02_Quest2.py
@@ -1,1 +1,29 @@
+#INPUTADRESS = "input\input_02.txt"
+#TESTGOAL = 0 #Level 1 Goal
+#TESTGOAL = 0 #Level 2 Goal
+INPUTADRESS = "input\input_test_02.txt"
+TESTGOAL = 1227775554 #Level 1 Testgoal
+#TESTGOAL = 0 #Level 2 Testgoal
 
+# Read input file
+def read_input():
+    with open(INPUTADRESS, 'r') as file:
+        data = file.read().split('\n')
+        return data
+
+# Count Sum
+def get_sum(input):
+    return 1
+
+# Main function
+def main():
+    input_data = read_input()
+    sum = get_sum(input_data)
+    print("input_data", input_data)
+    print(f"Sum: {sum}")
+    print(f"Powerlevel: {TESTGOAL} (expected)")
+    print(f"Difference: {sum - TESTGOAL}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day08_Quest1.py
@@ -0,0 +1,27 @@
+#INPUTADRESS = "input\input_08.txt"
+#TESTGOAL = 0 #Level 1 Goal
+INPUTADRESS = "input\input_test_08.txt"
+TESTGOAL = 1000 #Level 1 Testgoal
+
+# Read input file
+def read_input():
+    with open(INPUTADRESS, 'r') as file:
+        data = file.read().split('\n')
+        return data
+
+# Count Sum
+def get_sum(input):
+    return 1
+
+# Main function
+def main():
+    input_data = read_input()
+    sum = get_sum(input_data)
+    print("input_data", input_data)
+    print(f"Sum: {sum}")
+    print(f"Powerlevel: {TESTGOAL} (expected)")
+    print(f"Difference: {sum - TESTGOAL}")
+
+
+if __name__ == "__main__":
+    main()
